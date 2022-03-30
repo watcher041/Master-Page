ad23d3315872fd53613d95bda2ae0f25f454fd8c
watcher041 <watcher041@gmail.com>
2022-03-30 19:54:22 +0900

    localhost:3000にアクセスできるように変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn home() -> impl Responder {
+    "Hello,world!"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+        .route("/", web::get().to(home)))
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
 }
