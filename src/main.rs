d7c2d0d0c604a2d3d4b8fc724705f1e43d59484d
watcher041 <watcher041@gmail.com>
2022-04-16 19:41:16 +0900

    assetsを静的ファイル置き場に指定

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ mod routes;
 mod controllers;
 
 use tera::Tera;
+use actix_files::Files;
 use actix_web::{App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new()
+        .service(Files::new("/assets", ".").show_files_listing())
         .data( Tera::new("src/templates/**/*").unwrap() ) // teraの初期化
-        .configure( routes::routing ) )
-        .bind("localhost:3000")?
-        .run()
-        .await
+        .configure( routes::routing ) 
+    )
+    .bind("localhost:3000")?
+    .run()
+    .await
 }
